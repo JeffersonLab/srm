be01a734c76588152f9030e9e81979ceb60e72c9
slominskir <ryan.slominski@gmail.com>
2023-04-25 14:47:56 -0400

    Schema

--- docker/oracle/setup/03_default_data.sql
+++ docker/oracle/setup/03_default_data.sql
@@ -1,3 +1,3 @@
 alter session set container = XEPDB1;
 
-
+insert into SRM_OWNER.HCO_SETTINGS (HCO_SETTINGS_ID, GOAL_DATE, AUTO_EMAIL_YN, FEEDBACK_EMAIL_CSV, MASK_REQUEST_EMAIL_CSV, ACTIVITY_EMAIL_CSV) values (1, DATE '2013-11-22', 'N', null, 'tester@example.com', 'tester@example.com');
