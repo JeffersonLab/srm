35de651f31a72d3c2e88b48014541f07e033ee66
Ryan Slominski <Ryan.Slominski@gmail.com>
2025-03-27 16:21:29 -0400

    Use ContextRootController

--- src/main/java/org/jlab/srm/presentation/controller/ContextRootController.java
+++ src/main/java/org/jlab/srm/presentation/controller/ContextRootController.java
@@ -1,19 +1,19 @@
 package org.jlab.srm.presentation.controller;
 
-import java.io.IOException;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
 
 /**
  * @author ryans
  */
 @WebServlet(
-    name = "Index",
-    urlPatterns = {"/index"})
-public class Index extends HttpServlet {
+    name = "ContextRootController",
+    urlPatterns = {""})
+public class ContextRootController extends HttpServlet {
 
   /**
    * Handles the HTTP <code>GET</code> method.
@@ -27,6 +27,6 @@ public class Index extends HttpServlet {
   protected void doGet(HttpServletRequest request, HttpServletResponse response)
       throws ServletException, IOException {
 
-    response.sendRedirect(response.encodeRedirectURL(request.getContextPath() + "/readiness"));
+    response.sendRedirect(request.getContextPath() + "/readiness");
   }
 }
