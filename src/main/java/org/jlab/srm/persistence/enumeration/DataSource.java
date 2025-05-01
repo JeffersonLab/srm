d887e4fa5ab852f040e0bfb942cae3b56755069e
Ryan Slominski <Ryan.Slominski@gmail.com>
2025-05-01 09:14:30 -0400

    Use ace.jlab.org/ced

--- src/main/java/org/jlab/srm/persistence/enumeration/DataSource.java
+++ src/main/java/org/jlab/srm/persistence/enumeration/DataSource.java
@@ -4,10 +4,10 @@ package org.jlab.srm.persistence.enumeration;
  * @author ryans
  */
 public enum DataSource {
-  CED("ced.acc.jlab.org"),
+  CED("ace.jlab.org/ced"),
   LED("led.acc.jlab.org"),
   UED("ued.acc.jlab.org"),
-  INTERNAL("accweb.acc.jlab.org");
+  INTERNAL("ace.jlab.org");
   private final String hostname;
 
   DataSource(String hostname) {
